02640efb3c44eed85462b8c1794db1b90686535f
abuamar142 <amarjabon130@gmail.com>
2023-04-06 15:00:54 +0700

    add foreign key untuk user_id

--- users/models/user_actives.py
+++ users/models/user_actives.py
@@ -7,5 +7,7 @@ class UserActive(db.Model):
     user_id = sqlalchemy.Column(sqlalchemy.String(36), primary_key=True, nullable=False)
     access_token = sqlalchemy.Column(sqlalchemy.String(32))
 
+    role_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('auth_users.user_id'), nullable=False)
+
     def __repr__(self):
         return '<UserActive %s>' % self.user_id
