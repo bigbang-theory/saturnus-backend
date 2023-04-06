13bfaa1d92a410b17b934b7a2d230de44b104559
abuamar142 <amarjabon130@gmail.com>
2023-04-06 15:03:38 +0700

    ubah model dari user_id ke foreign key

--- users/models/user_actives.py
+++ users/models/user_actives.py
@@ -4,10 +4,8 @@ import sqlalchemy
 class UserActive(db.Model):
     __tablename__ = 'user_actives'
 
-    user_id = sqlalchemy.Column(sqlalchemy.String(36), primary_key=True, nullable=False)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('auth_users.user_id'), nullable=False)
     access_token = sqlalchemy.Column(sqlalchemy.String(32))
-
-    role_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('auth_users.user_id'), nullable=False)
 
     def __repr__(self):
         return '<UserActive %s>' % self.user_id
