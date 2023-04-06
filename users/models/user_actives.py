8ab27839069e999c528180bd6fd4fcf9caa3e84c
abuamar142 <amarjabon130@gmail.com>
2023-04-06 13:58:42 +0700

    delete default and add nullable false to user_id

--- users/models/user_actives.py
+++ users/models/user_actives.py
@@ -1,11 +1,10 @@
 from ..main import db
 import sqlalchemy
-import uuid
 
 class UserActive(db.Model):
     __tablename__ = 'user_actives'
 
-    user_id = sqlalchemy.Column(sqlalchemy.String(36), primary_key=True, default=str(uuid.uuid4()))
+    user_id = sqlalchemy.Column(sqlalchemy.String(36), primary_key=True, nullable=False)
     access_token = sqlalchemy.Column(sqlalchemy.String(32))
 
     def __repr__(self):
